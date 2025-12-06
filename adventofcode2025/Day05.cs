d406b7ea2c9bc7ecbae820b449a5cdd74ea3b8dd
Andreas Amberg <andreasamberg@gmail.com>
2025-12-06 13:15:52 +0100

    Part 2 added. All this needs some refactoring, but thats for later.

--- adventofcode2025/Day05.cs
+++ adventofcode2025/Day05.cs
@@ -15,17 +15,8 @@ namespace adventofcode2025
 
         public void SolvePart1()
         {
-            readFileAnTransform();
-            Console.WriteLine("Result Part1: "+ freshItems.Count);
-        }
-
-        public void SolvePart2()
-        {
-            Console.WriteLine("Result Part2: ");
-        }
-
-        private void readFileAnTransform()
-        {
+            freshItems.Clear();
+            freshItemRanges.Clear();
             using StreamReader reader = new("files/Day05Input.txt");
 
             // Read the stream as a string.
@@ -59,7 +50,76 @@ namespace adventofcode2025
                     }
                 }
             }
+
+            Console.WriteLine("Result Part1: "+ freshItems.Count);
         }
+
+        public void SolvePart2()
+        {
+            freshItems.Clear();
+            freshItemRanges.Clear();
+            using StreamReader reader = new("files/Day05Input.txt");
+
+            // Read the stream as a string.
+            string text = reader.ReadToEnd();
+            string[] lines = text.Split("\n");
+
+            bool emtpyLineFound = false;
+            foreach (string line in lines)
+            {
+                if (line == "")
+                {
+                    emtpyLineFound = true;
+                    break;
+                }
+
+                if (!emtpyLineFound)
+                {
+                    string[] parts = line.Split("-");
+
+                    double from = double.Parse(parts[0]);
+                    double to = double.Parse(parts[1]);
+
+                    freshItemRanges.Add((from, to));
+                }
+            }
+
+            freshItemRanges = freshItemRanges.OrderBy(x => x.from).ToList();
+
+            List<(double from, double to)> optimizedFreshRanges = new List<(double from, double to)>();
+            optimizedFreshRanges.Add(freshItemRanges[0]);
+
+            bool isFirst = true;
+            foreach (var (start, end) in freshItemRanges) 
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                    continue;
+                }
+
+                var lastOptimizedRow = optimizedFreshRanges[optimizedFreshRanges.Count-1];
+                if (start <= lastOptimizedRow.to)
+                {
+                    optimizedFreshRanges[optimizedFreshRanges.Count - 1] = (lastOptimizedRow.from, Math.Max(end, lastOptimizedRow.to));
+                }
+                else
+                {
+                    optimizedFreshRanges.Add((start, end));
+                }
+
+                var b = 0;
+            }
+
+            double countFreshItems = 0;
+            foreach (var (start, end) in optimizedFreshRanges)
+            {
+                countFreshItems = countFreshItems + end - start + 1;
+            }
+
+            Console.WriteLine("Result Part2: " + countFreshItems);
+        }
+
 
         private bool checkIfItemIsFresh(double item)
         {
