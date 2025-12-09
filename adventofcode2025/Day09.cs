ac8f9bd08de58ff7cf9f4015ed52510e7023580b
Andreas Amberg <andreasamberg@gmail.com>
2025-12-09 17:54:09 +0100

    Added Day09 Part1

---
+++ adventofcode2025/Day09.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2025
+{
+    internal class Day09 : IDay
+    {
+
+        public void SolvePart1()
+        {
+            using StreamReader reader = new("files/Day09Input.txt");
+            string text = reader.ReadToEnd();
+            string[] lines = text.Split("\n");
+
+            List<(double xCord, double yCord)> redTiles = new List<(double xCord, double yCord)>();
+
+            double maxTileSite = 0;
+            foreach (string line in lines)
+            {
+                (double xPos, double yPos) = Deconstruct(line.Split(","));
+                foreach (var (tmpX, tmpY) in redTiles)
+                {
+                    double size = (Math.Abs(xPos - tmpX) + 1) * (Math.Abs(yPos - tmpY) + 1);
+                    maxTileSite = Math.Max(maxTileSite, size);
+                }
+
+                redTiles.Add((xPos, yPos));
+            }
+
+            Console.WriteLine("Result Part1: " + maxTileSite);
+        }
+
+        public void SolvePart2()
+        {
+            Console.WriteLine("Result Part2: ");
+        }
+
+        private (double, double) Deconstruct(string[] parts)
+        {
+            return (double.Parse(parts[0]), double.Parse(parts[1]));
+        }
+    }
+}
