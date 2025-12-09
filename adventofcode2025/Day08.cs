7c79f0ab661da475b9f4839f278775aec515d856
Andreas Amberg <andreasamberg@gmail.com>
2025-12-09 17:23:17 +0100

    added Day08 Part2 could use some refactoring

--- adventofcode2025/Day08.cs
+++ adventofcode2025/Day08.cs
@@ -99,7 +99,80 @@ namespace adventofcode2025
 
         public void SolvePart2()
         {
-            Console.WriteLine("Result Part2: ");
+            using StreamReader reader = new("files/Day08Input.txt");
+            string text = reader.ReadToEnd();
+            string[] lines = text.Split("\n");
+
+            List<(int xCord, int yCord, int zCord)> boxes = new List<(int xCord, int yCord, int zCord)>();
+            List<(int indexLow, int indexHigh, double distance)> sorter = new List<(int indexLow, int indexHigh, double distance)>();
+
+            int counter = 0;
+
+            foreach (string line in lines)
+            {
+                string[] splitCoords = line.Split(",");
+                boxes.Add((int.Parse(splitCoords[0]), int.Parse(splitCoords[1]), int.Parse(splitCoords[2])));
+
+                for (int tmpI = 0; tmpI < counter; tmpI++)
+                {
+                    double distance = calcDistance(boxes[tmpI].xCord, boxes[tmpI].yCord, boxes[tmpI].zCord, boxes[counter].xCord, boxes[counter].yCord, boxes[counter].zCord);
+                    sorter.Add((tmpI, counter, distance));
+                }
+
+                counter++;
+            }
+
+            sorter = sorter.OrderBy(x => x.distance).ToList();
+
+            List<int[]> circuits = new List<int[]>();
+            int result = 0;
+
+            foreach (var (indexLow, indexHigh, distance) in sorter)
+            {
+                int[] tmpBox = new int[] { indexLow, indexHigh };
+
+                if (circuits.FindIndex(x => tmpBox.All(x.Contains)) >= 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    List<int[]> all = circuits.FindAll(x => tmpBox.Any(x.Contains));
+
+                    if (all.Count == 2)
+                    {
+                        int c0 = circuits.FindIndex(x => all[0].All(x.Contains));
+                        int c1 = circuits.FindIndex(x => all[1].All(x.Contains));
+                        tmpBox = circuits[c0].Union(circuits[c1]).ToArray();
+
+                        if (c0 > c1)
+                        {
+                            circuits.RemoveAt(c0);
+                            circuits.RemoveAt(c1);
+                        }
+                        else
+                        {
+                            circuits.RemoveAt(c1);
+                            circuits.RemoveAt(c0);
+                        }
+                    }
+                    else if (all.Count == 1)
+                    {
+                        int c = circuits.FindIndex(x => all[0].All(x.Contains));
+                        tmpBox = circuits[c].Union(tmpBox).ToArray();
+                        circuits.RemoveAt(c);
+                    }
+                }
+
+                circuits.Add(tmpBox);
+                if (circuits.Count == 1 && circuits[0].Count() == boxes.Count)
+                {
+                    result = boxes[indexLow].xCord * boxes[indexHigh].xCord;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Result Part2: " + result);
         }
     }
 }
