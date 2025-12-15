d17b578a6af6e2344a48c3dda9a415a1553e4833
Andreas Amberg <andreasamberg@gmail.com>
2025-12-15 22:23:54 +0100

    no solution so far for Day12, just add stuff

---
+++ adventofcode2025/Day12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode2025
+{
+    internal class Day12
+    {
+        public void SolvePart1()
+        {
+            Console.WriteLine("Result Part1: ");
+        }
+
+        public void SolvePart2()
+        {
+            Console.WriteLine("Result Part1: ");
+        }
+    }
+}
