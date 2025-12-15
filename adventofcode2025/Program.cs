aed97d88590453d31fa567133173385dd8be291e
Andreas Amberg <andreasamberg@gmail.com>
2025-12-15 21:44:24 +0100

    back To dayselect

--- adventofcode2025/Program.cs
+++ adventofcode2025/Program.cs
@@ -30,7 +30,7 @@ static void Main()
     }
 
     var selectedDay = implementedDays[implementedDays.Length-1];
-    selectedDay = "Day02";
+    //selectedDay = "Day03";
 
     if (Environment.GetEnvironmentVariable("DOTNET_MODIFIABLE_ASSEMBLIES") != "debug")
     {
