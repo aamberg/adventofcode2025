c7ccf2bd8f46cc990ade4fdc7ba2664de53c0919
Andreas Amberg <andreasamberg@gmail.com>
2025-12-16 18:09:07 +0100

    added structure of day12 license modified

--- adventofcode2025/Day12.cs
+++ adventofcode2025/Day12.cs
@@ -6,10 +6,85 @@ using System.Threading.Tasks;
 
 namespace adventofcode2025
 {
-    internal class Day12
+    internal class Day12 : IDay
     {
+        private class PuzzlePiece
+        {
+            public string[,] grid { get; set; }
+        }
+        private class QuizRow
+        {
+            public int dimensionX { get; set; }
+            public int dimensionY { get; set; }
+
+            public List<int> counters { get; set; }
+        }
+
+        private static List<PuzzlePiece> pieces = new List<PuzzlePiece>();
+
+        private static List<QuizRow> quizRows = new List<QuizRow>();
+
         public void SolvePart1()
         {
+            using StreamReader reader = new("files/TestDay12Input.txt");
+            string text = reader.ReadToEnd();
+            string[] lines = text.Split("\n");
+
+            int tmpIndex = 0;
+            List<string> tmpGrid = new List<string>();
+
+            foreach (string line in lines)
+            {
+                if (line.EndsWith(":"))
+                {
+                    tmpIndex = int.Parse(line.Substring(0, line.Length - 1));
+                    continue;
+                }
+                else if (line == "")
+                {
+                    string[,] strGrid = new string[tmpGrid[0].Length, tmpGrid.Count()];
+                    for (int y = 0; y < tmpGrid.Count(); y++)
+                    {
+                        for (int x = 0; x < tmpGrid[y].Length; x++)
+                        {
+                            strGrid[y, x] = tmpGrid[y][x].ToString();
+                        }
+                    }
+
+                    PuzzlePiece piece = new PuzzlePiece();
+                    piece.grid = strGrid;
+                    pieces.Add(piece);
+
+                    tmpGrid.Clear();
+                }
+                else if (!line.EndsWith(":") && line.Contains(":"))
+                {
+                    // checker
+                    string[] parts = line.Split(" ");
+                    string dimensions = parts[0].Substring(0, parts[0].Length - 1);
+
+                    List<int> tmpCounters = new List<int>();
+                    foreach (string counter in parts.Skip(1))
+                    {
+                        tmpCounters.Add(int.Parse(counter));
+                    }
+
+                    string[] splitDimension = dimensions.Split("x");
+
+                    QuizRow tmpRow = new QuizRow();
+                    tmpRow.dimensionX = int.Parse(splitDimension[0]);
+                    tmpRow.dimensionY = int.Parse(splitDimension[1]);
+                    tmpRow.counters = tmpCounters;
+
+                    quizRows.Add(tmpRow);
+                }
+                else
+                {
+                    tmpGrid.Add(line);
+                }
+            }
+
+
             Console.WriteLine("Result Part1: ");
         }
 
